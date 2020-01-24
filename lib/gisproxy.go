d9ad82f1b110a8e710ea5d5d8893d1fde4422830
Mathieu MAST <mmast@localhost.localdomain>
2020-01-24 17:02:54 +0100

    Use default parameters in http.Transport

--- lib/gisproxy.go
+++ lib/gisproxy.go
@@ -6,9 +6,11 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"regexp"
 	"strings"
+	"time"
 )
 
 // BeforeSend defines before send callback function
@@ -49,7 +51,18 @@ func NewGisProxy(prefix string) *GisProxy {
 	// create http client
 	gp.client = &http.Client{}
 	gp.client.Transport = &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
 	}
 	return gp
 }
